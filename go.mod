3ce641cc33ad0ff166862815e680213e80f2fea5
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2024-07-17 01:26:04 +0800

    forgot to stage all the changes oops

--- go.mod
+++ go.mod
@@ -5,10 +5,15 @@ go 1.22.5
 require (
 	github.com/bwmarrin/discordgo v0.28.1
 	github.com/joho/godotenv v1.5.1
+	github.com/psykhi/wordclouds v0.0.0-20231014190151-b9dd58fabbef
 )
 
 require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/image v0.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 )
--- main.go
+++ main.go
@@ -2,48 +2,112 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	"image/png"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	"github.com/psykhi/wordclouds"
 )
 
 func main() {
-	fmt.Println("wordulous")
+	fmt.Println("Wordulous is online")
+
+	// Load .env file for the bot access token
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error while loading .env file: ")
 	}
 	BOT_ACCESS_TOKEN := os.Getenv("DISCORD_BOT_ACCESS_TOKEN")
 
+	//	Initialize a new discord bot session using the access token
 	session, err := discordgo.New("Bot " + BOT_ACCESS_TOKEN)
-
 	if err != nil {
 		log.Fatal("Error while creating discord session: ", err)
 	}
 
 	//session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate))
-
+	//	Open/Stat the discord session
 	err = session.Open()
-
 	if err != nil {
 		log.Fatal("Error while opening discord session: ", err)
 	}
-
 	defer session.Close()
 
 	// GAME disc chat channel id: 844133446438748171
-	msgs, err := session.ChannelMessages("844133446438748171", 5, "", "", "")
-
+	msgs, err := session.ChannelMessages("844133446438748171", 100, "", "", "")
 	if err != nil {
-		log.Fatal("Discord message retrieval failure")
+		log.Fatal("Discord message retrieval failure", err)
 	}
 
-	for index, element := range msgs {
-		fmt.Println("message no. ", index)
-		fmt.Println(element.Author.Username, ": ", element.ContentWithMentionsReplaced())
+	dict := map[string]int{}
+	corrupted := 0
+
+	for _, msg := range msgs {
+		content, err := msg.ContentWithMoreMentionsReplaced(session)
+		if err != nil {
+			corrupted++
+			continue
+		}
+
+		//remove all non-alphanumeric characters
+		//convert everything to lowercase
+		content = strings.ToLower(regexp.MustCompile(`[^a-zA-Z0-9\t ]`).ReplaceAllString(content, ""))
+
+		//tokenize each message into words
+		tokens := strings.Fields(content)
+
+		for _, word := range tokens {
+			count := dict[word]
+			if count == 0 {
+				dict[word] = 1
+				continue
+			}
+			dict[word]++
+		}
 	}
 
-	fmt.Println("Wordulous is online")
+	fmt.Print(dict)
+
+	w_cloud := wordclouds.NewWordcloud(
+		dict,
+		wordclouds.FontFile("fonts/MontserratBlack.ttf"),
+		wordclouds.Height(2048),
+		wordclouds.Width(2048),
+		wordclouds.Colors([]color.Color{
+			color.RGBA{0xFF, 0x00, 0x00, 0xFF}, // Red
+			color.RGBA{0x00, 0xFF, 0x00, 0xFF}, // Green
+			color.RGBA{0x00, 0x00, 0xFF, 0xFF}, // Blue
+		}),
+		wordclouds.BackgroundColor(color.RGBA{0xFF, 0xFF, 0xFF, 0xFF}), // White background
+	)
+
+	// Draw the word cloud
+	img := w_cloud.Draw()
+
+	// Save the image to the pc
+	f, err := os.Create("output.png")
+	if err != nil {
+		log.Fatal("Error while creating outpet image file: ", err)
+	}
+	defer f.Close()
+
+	err = png.Encode(f, img)
+	if err != nil {
+		log.Fatal("PNG Encoding error: ", err)
+	}
+
+	// for _, msg := range msgs {
+	// 	fmt.Println(msg.Author.Username, ": ")
+	// 	content, err := msg.ContentWithMoreMentionsReplaced(session)
+	// 	if err != nil {
+	// 		log.Fatal("Message content with more mentions replaced error: ", err)
+	// 	}
+	// 	fmt.Println(content)
+	// }
+
 }
